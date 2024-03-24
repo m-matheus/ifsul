94c984f72f7b5be907f5a0a337b9f2de060c05b2
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-03-24 12:44:09 -0300

    exercicios proposto em aula

---
+++ terceiro-semestre/di-2/ex1/script.js
@@ -0,0 +1,4 @@
+let frase = prompt("Escreva uma frase: ");
+let numPalavras = frase.split(" ");
+console.log(numPalavras.length);
+alert(`O número de palavras é: ${numPalavras.length}`);
---
+++ terceiro-semestre/di-2/ex2/script.js
@@ -0,0 +1,6 @@
+let frase = prompt("Escreva uma frase: ");
+let palavra1 = (prompt("Escreva uma palavra para substituir: "));
+let palavra2 = (prompt("Esreva a palavra que irá substituir no lugar: "));
+
+alert(frase.replaceAll(palavra1, palavra2));
+
