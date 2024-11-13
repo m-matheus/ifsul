69681d42dfb36d423520f9ae222cccb570bde1dd
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-11-13 09:13:26 -0300

    fix: paths

--- quarto-semestre/fundamentos-segurança/w1/demo/src/main/java/com/matheus/demo/decrypt/Main.java
+++ quarto-semestre/fundamentos-segurança/w1/demo/src/main/java/com/matheus/demo/decrypt/Main.java
@@ -12,33 +12,31 @@ import javax.crypto.spec.*;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Carregar a chave privada RSA
-            // PrivateKey privateKey = loadPrivateKey("../files/sua_chave_privada.pem");
             KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
             keyGen.initialize(2048);
             KeyPair keyPair = keyGen.generateKeyPair();
             PrivateKey privateKey = keyPair.getPrivate();
             
-            // Ler o arquivo de chave cifrada                         caminho/para/SUAMATRICULA_key.enc
-            byte[] encryptedKey = Files.readAllBytes(Paths.get("../files/keys_enc/20231SS.ADS_J0003_key.enc"));
+            // Ler o arquivo de chave cifrada
+            byte[] encryptedKey = Files.readAllBytes(Paths.get("C:\\Users\\Mmatt\\OneDrive\\Documentos\\GitHub\\ifsul\\quarto-semestre\\fundamentos-segurança\\w1\\demo\\src\\main\\java\\com\\matheus\\demo\\files/keys_enc/20231SS.ADS_J0003_key.enc"));
 
             // Decifrar a chave simétrica usando RSA
             byte[] symmetricKey = decryptRSA(encryptedKey, privateKey);
 
             // Configurar os parâmetros para Chacha20 a partir do arquivo 'dados.txt'
-            byte[] nonce = readNonceFromFile("../files/dados.txt");
+            byte[] nonce = readNonceFromFile("C:\\Users\\Mmatt\\OneDrive\\Documentos\\GitHub\\ifsul\\quarto-semestre\\fundamentos-segurança\\w1\\demo\\src\\main\\java\\com\\matheus\\demo\\files/dados.txt");
             Cipher chacha20 = Cipher.getInstance("ChaCha20");
             SecretKeySpec keySpec = new SecretKeySpec(symmetricKey, "ChaCha20");
 
             // Inicializar o cipher
             chacha20.init(Cipher.DECRYPT_MODE, keySpec, new IvParameterSpec(nonce));
 
-            // Ler o arquivo cifrado e decifrar                       caminho/para/SUAMATRICULA_msgfile.enc
-            byte[] encryptedMsg = Files.readAllBytes(Paths.get("../files/msg_fil_enc/20231SS.ADS_J0003_msgfile.enc"));
+            // Ler o arquivo cifrado e decifrar
+            byte[] encryptedMsg = Files.readAllBytes(Paths.get("C:\\Users\\Mmatt\\OneDrive\\Documentos\\GitHub\\ifsul\\quarto-semestre\\fundamentos-segurança\\w1\\demo\\src\\main\\java\\com\\matheus\\demo\\files/msg_fil_enc/20231SS.ADS_J0003_msgfile.enc"));
             byte[] decryptedMsg = chacha20.doFinal(encryptedMsg);
 
             // Salvar o conteúdo decifrado em um arquivo .txt
-            String outputFilePath = "../files/saida_decifrada.txt";
+            String outputFilePath = "C:\\Users\\Mmatt\\OneDrive\\Documentos\\GitHub\\ifsul\\quarto-semestre\\fundamentos-segurança\\w1\\demo\\src\\main\\java\\com\\matheus\\demo\\files/saida_decifrada.txt";
             Files.write(Paths.get(outputFilePath), decryptedMsg);
 
             // Imprimir a confirmação e o conteúdo decifrado
